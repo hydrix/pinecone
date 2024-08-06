29d9a0ea273054d26c98bb8ec896749448524021
= <jargalhuujaga@gmail.com>
2024-08-06 18:02:47 +0800

    delete

--- game/canvas.js
+++
@@ -1,9 +0,0 @@
-let canvas = document.getElementById("canvas");
-var window_height = window.innerHeight;
-var window_width = window.innerWidth;
-
-canvas.width = window_width; 
-
-canvas.height = window_height;
-
-canvas.style.background = "#ff8";
